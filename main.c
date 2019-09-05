3def8c93606e4e5d02f5f910bd7121c61cb73cbb
Element Green <element@elementsofsound.org>
2019-09-04 21:17:13 -0600

    malloc was not allocating proper array size. Added memset to initialize memory, though it is useless dummy code anyways.

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 
 #include "fmv-test.h"
 
@@ -10,7 +11,8 @@ main (int argc, char **argv)
 {
   double *dArray;
 
-  dArray = malloc (ARRAY_SIZE);
+  dArray = malloc (ARRAY_SIZE * sizeof (double));
+  memset (dArray, 0, ARRAY_SIZE * sizeof (double));
 
   fmv_test (dArray, ARRAY_SIZE);
 
